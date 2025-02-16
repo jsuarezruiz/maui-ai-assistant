e3512c3a9fc31a59744c8eb34a3b1d5089962cb8
Javier Suárez <javiersuarezruiz@hotmail.com>
2025-02-16 12:37:52 +0100

    Improve command to migrate from XF

--- src/Commands/ConvertFromXamarinForms.cs
+++ src/Commands/ConvertFromXamarinForms.cs
@@ -5,7 +5,7 @@
     {
         protected override async Task ExecuteAsync(OleMenuCmdEventArgs e)
         {
-            SystemMessage = "Refactor this code converting it from Xamarin.Forms to .NET MAUI. Return only the refactored code.";
+            SystemMessage = "Refactor this code converting it from Xamarin.Forms to .NET MAUI based on https://learn.microsoft.com/en-us/dotnet/maui/migration/multi-project-to-multi-project. Return only the refactored code.";
             CommandBehavior = CommandBehavior.Replace;
 
             await base.ExecuteAsync(e);
